ec4b5c78450b6ce125483050ca6437a0c97b06c6
BendikAlvik <105675734+BendikAlvik@users.noreply.github.com>
2023-11-01 14:08:33 +0100

    La inn update_employee og gjorde my.dao litt mer leselig

--- INFO212 kode/Oblig 4/my_dao.py
+++ INFO212 kode/Oblig 4/my_dao.py
@@ -4,9 +4,10 @@ import json
 URI = "neo4j+ssc://f05d33fd.databases.neo4j.io"
 AUTH = ("neo4j", "52cQIHi4QVIsIyNJig4s5rnx_4DccVqql4LbJPYOYy8")
 
+
 def _get_connection() -> Driver:
     # driver = GraphDatabase.driver(URI, auth=AUTH)
-    driver = GraphDatabase.driver(URI,auth=AUTH)
+    driver = GraphDatabase.driver(URI, auth=AUTH)
     driver.verify_connectivity()
     return driver
 
@@ -16,12 +17,14 @@ def node_to_json(node):
     return node_properties
 
 
+# Car
 def findAllCars():
     with _get_connection().session() as session:
         cars = session.run("MATCH (a:Car) RETURN a;")
         nodes_json = [node_to_json(record["a"]) for record in cars]
         print(nodes_json)
         return nodes_json
+
 
 def findCarByReg(reg):
     with _get_connection().session() as session:
@@ -31,41 +34,53 @@ def findCarByReg(reg):
         print(nodes_json)
         return nodes_json
 
+
 def save_car(make, model, reg, year, capacity, status):
-    cars = _get_connection().execute_query("MERGE (a:Car{make: $make, model: $model, reg: $reg,year: $year, capacity:$capacity, status: $status}) RETURN a;", make = make, model = model, reg = reg, year = 
-    year, capacity = capacity, status = status)
+    cars = _get_connection().execute_query("MERGE (a:Car{make: $make, model: $model, reg: $reg,year: $year, capacity:$capacity, status: $status}) RETURN a;",
+                                           make=make, model=model, reg=reg, year=year, capacity=capacity, status=status)
     nodes_json = [node_to_json(record["a"]) for record in cars]
     print(nodes_json)
     return nodes_json
 
+
 def update_car(make, model, reg, year, capacity, status):
     with _get_connection().session() as session:
         cars = session.run("MATCH (a:Car{reg:$reg}) set a.make=$make, a.model=$model, a.year = $year, a.capacity = $capacity, a.status = $status RETURN a;", reg=reg, make=make, model=model, year=year,
-        capacity=capacity, status=status)
+                           capacity=capacity, status=status)
         print(cars)
         nodes_json = [node_to_json(record["a"]) for record in cars]
         print(nodes_json)
         return nodes_json
 
-def delete_car(reg):
-    _get_connection().execute_query("MATCH (a:Car{reg: $reg}) delete a;", reg =reg)
 
+def delete_car(reg):
+    _get_connection().execute_query(
+        "MATCH (a:Car{reg: $reg}) delete a;", reg=reg)
+
+
+# Customer
 def customer(name, age, adress):
-    customer = _get_connection().execute_query("MERGE (a:Customer{name: $name, age: $age, adress: $adress}) RETURN a;", name = name, age = age, adress = adress)
+    customer = _get_connection().execute_query(
+        "MERGE (a:Customer{name: $name, age: $age, adress: $adress}) RETURN a;", name=name, age=age, adress=adress)
     nodes_json = [node_to_json(record["a"]) for record in customer]
     print(nodes_json)
     return nodes_json
 
+
 def update_customer(name, age, adress):
     with _get_connection().session() as session:
-        customers = session.run("MATCH (a:Customer{name:$name}) set a.name=$name, a.age=$age, a.adress = $adress RETURN a;", name=name, age=age, adress=adress)
+        customers = session.run(
+            "MATCH (a:Customer{name:$name}) set a.name=$name, a.age=$age, a.adress = $adress RETURN a;", name=name, age=age, adress=adress)
         print(customers)
         nodes_json = [node_to_json(record["a"]) for record in customers]
         print(nodes_json)
         return nodes_json
 
+
 def delete_customer(name):
-    _get_connection().execute_query("MATCH (a:Customer{name: $name}) delete a;", name = name)
+    _get_connection().execute_query(
+        "MATCH (a:Customer{name: $name}) delete a;", name=name)
+
 
 def findAllCustomers():
     with _get_connection().session() as session:
@@ -74,11 +89,25 @@ def findAllCustomers():
         print(nodes_json)
         return nodes_json
 
+
+# Employee
 def employee(name, age, adress, branch):
-    employee = _get_connection().execute_query("MERGE (a:Employee{name: $name, age: $age, adress: $adress, branch: $branch}) RETURN a;", name = name, age = age, adress = adress, branch = branch)
+    employee = _get_connection().execute_query(
+        "MERGE (a:Employee{name: $name, age: $age, adress: $adress, branch: $branch}) RETURN a;", name=name, age=age, adress=adress, branch=branch)
     nodes_json = [node_to_json(record["a"]) for record in employee]
     print(nodes_json)
     return nodes_json
+
+
+def update_employee(name, age, adress, branch):
+    with _get_connection().session() as session:
+        employees = session.run(
+            "MATCH (a:Employee{name:$name}) set a.name=$name, a.age=$age, a.adress=$adress, a.branch=$branch RETURN a;", name=name, age=age, adress=adress, branch=branch)
+        print(employees)
+        nodes_json = [node_to_json(record["a"]) for record in employees]
+        print(nodes_json)
+        return nodes_json
+
 
 def findAllEmployees():
     with _get_connection().session() as session:
@@ -87,15 +116,17 @@ def findAllEmployees():
         print(nodes_json)
         return nodes_json
 
+
 def delete_employee(name):
-    _get_connection().execute_query("MATCH (a:Employee{name: $name}) delete a;", name = name)
+    _get_connection().execute_query(
+        "MATCH (a:Employee{name: $name}) delete a;", name=name)
 
 
 # result = save_car(make="Toyota", model="Camry", reg="XYZ123", year=2023, capacity=5)
 # print(result)
 
-#customer(name='Per Hansen', age='24', adress='Skolegaten 1b, 2348 Skolebyem')
-#employee(name='Thor Thodesen', age='39', adress='Bilveien 23, 3929 LangVekkIStan', branch='Bergen')
+# customer(name='Per Hansen', age='24', adress='Skolegaten 1b, 2348 Skolebyem')
+# employee(name='Thor Thodesen', age='39', adress='Bilveien 23, 3929 LangVekkIStan', branch='Bergen')
 
 # update_car(reg='ZZZ123', year='2013', model='A3', make='Audi', capacity='5', status='Available')
 
